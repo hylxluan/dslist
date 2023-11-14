a3300275c49b8e53101ec0a50f8f73980a94df03
hylxluan <luanplays212004@gmail.com>
2023-11-13 23:25:44 -0300

    Adding entity's models and repository

---
+++ src/main/java/com/dslist/dslist/repositories/GameRepository.java
@@ -0,0 +1,10 @@
+package com.dslist.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.dslist.dslist.entities.Game;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long>{
+
+}
