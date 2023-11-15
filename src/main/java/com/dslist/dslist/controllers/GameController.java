477c4cfdb2a1ddfc07928f326ff4ab17ac970e86
hylxluan <luanplays212004@gmail.com>
2023-11-15 00:27:31 -0300

    Creating H2 DB and Controllers

---
+++ src/main/java/com/dslist/dslist/controllers/GameController.java
@@ -0,0 +1,12 @@
+package com.dslist.dslist.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/games")
+
+
+public class GameController {
+
+}
